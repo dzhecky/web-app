cba5527f972072c13ac84d919b268364ec7e8707
rikimuhammadasli021299 <rikimuhammadasli@gmail.com>
2023-11-23 16:40:10 +0700

    upadate page register

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 // pages
 import Landing from './pages/Landing';
-// import Register from './pages/register';
+import Register from './pages/register';
 import Login from './pages/Login';
 import ForgotPassword from './pages/ForgotPassword';
 import Home from './pages/Home';
@@ -17,7 +17,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Landing />} />
-          {/* <Route path='/register' element={<Register />} /> */}
+          <Route path='/register' element={<Register />} />
           <Route path='/login' element={<Login />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/home' element={<Home />} />
